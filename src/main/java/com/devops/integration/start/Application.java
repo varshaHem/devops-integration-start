48f8caa63941fd3c3d41ffc958fd9855a3b86a2e
varshaHem <38180635+varshaHem@users.noreply.github.com>
2018-04-14 13:10:24 +0100

    Update Application.java
    Jenkins test

--- src/main/java/com/devops/integration/start/Application.java
+++ src/main/java/com/devops/integration/start/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main (String[] args){
+        System.out.println("Jenkins Test!");
         SpringApplication.run(Application.class, args);
     }
 }
