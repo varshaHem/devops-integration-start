46faa299e2349b7dd094992462d9b23a887ae59b
unknown <varshathnk@gmail.com>
2018-04-14 12:04:54 +0100

    Initial setup

---
+++ src/main/java/com/devops/integration/start/Application.java
@@ -0,0 +1,11 @@
+package com.devops.integration.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main (String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
